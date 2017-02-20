92831e73556e8c1010c968b439c32daf77589a32
Paul Morris <paul@paulwmorris.com>
2017-02-20 11:57:38 -0500

    Omit mtime from test assertion

--- tests/test_py1.py
+++ tests/test_py1.py
@@ -1,6 +1,11 @@
-import unittest, subprocess, tempfile, os
+import unittest, subprocess, tempfile, os, csv
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+
+# takes a csv file path and returns a list of dicts of the csv data
+def readCSV(file_path):
+    with open(file_path) as f:
+        return list(csv.DictReader(f))
 
 class py1Test(unittest.TestCase):
 
@@ -10,10 +15,12 @@ class py1Test(unittest.TestCase):
         _, path_temp = tempfile.mkstemp()
         subprocess.run(["python3", "py1-csv-from-repo.py", "tests/data/test-repo", "-o", path_temp])
 
-        with open(os.path.join(DATA_DIR, "expected-csv-for-test-repo", "py1-output.csv")) as f:
-            expected = f.read()
+        expected = readCSV(os.path.join(DATA_DIR, "expected-csv-for-test-repo", "py1-output.csv"))
+        result = readCSV(path_temp)
 
-        with open(path_temp) as f:
-            result = f.read()
+        for d in expected:
+            d['mtime'] = None
+        for d in result:
+            d['mtime'] = None
 
         self.assertEqual(expected, result)
