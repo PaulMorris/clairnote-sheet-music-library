c6889935743c51d84f21ad37881f129c3df51fe5
Paul Morris <paul@paulwmorris.com>
2017-03-23 11:58:31 -0400

    Refactor script for collecting data and generating a csv file

--- py1-csv-from-repo.py
+++ py1-csv-from-repo.py
@@ -5,26 +5,18 @@ from py_ly_parsing import (regexes, regex_search, vsn_greater_than_or_equals,
     get_header_data, check_for_clairnote_code, get_most_recent_mtime)
 from py_csv_merging import merge_csv_data
 
-# walks through a directory and all subdirectories creating a csv file with data from the ly files
+# walks through a directory and subdirectories creating a csv file with data from the ly files
 
 # COMMAND LINE ARGUMENTS
 parser = argparse.ArgumentParser()
-parser.add_argument("rootdir", help="The root directory that contains the ly files") # e.g. '../../The-Mutopia-Project/ftp/' or 'test-ly-files'
-parser.add_argument("-o", "--csv-output", help="Path and name for the CSV file output") # e.g. 'out/from-repo.csv'
-parser.add_argument("-p", "--csv-previous", help="Path and name of the previous CSV file") # e.g. 'out/previous-final.csv'
-parser.add_argument("-l", "--earliest-ly-version", help="The earliest LilyPond version to include in CSV file") # e.g. '2.14.0'
-parser.add_argument("-m", "--mode", help="The mode for parsing ly files, e.g. 'mutopia' or 'thesession'")
-
-csv_keys_mutopia = ['mutopia-id', 'parse-order', 'omit?', 'omit-reason', 'new?', 'error-status?', 'flagged?',
-    'cn-code', 'ly-version', 'mutopiacomposer', 'cn-title', 'cn-opus', 'path', 'filename',
-    'cn-style', 'cn-instrument',
-    'cn-poet', 'license-type', 'license-vsn', 'cn-license', 'mtime',
-    'arranger', 'date', 'source', 'copyright', 'tagline',
-    'footer', 'composer', 'mutopiatitle', 'title', 'mutopiaopus', 'opus', 'mutopiastyle', 'style',
-    'mutopiainstrument', 'instrument', 'mutopiapoet', 'poet', 'mutopialicense', 'license']
+parser.add_argument("mode", help = "The mode for parsing ly files, e.g. 'mutopia' or 'thesession'")
+parser.add_argument("rootdir", help="The root directory that contains the ly files")
+parser.add_argument("-o", "--csv-output", help="Path and file name for the CSV file output")
+parser.add_argument("-p", "--csv-previous", help="Path and file name of the previous CSV file")
+parser.add_argument("-l", "--earliest-ly-version", help="The earliest LilyPond version to include in CSV file (e.g. '2.14.0')")
 
 
-''' example of the session files' header fields:
+''' example of header fields in 'the session' files:
 \header {
 	crossRefNumber = "1"
 	footnotes = ""
@@ -32,20 +24,31 @@ csv_keys_mutopia = ['mutopia-id', 'parse-order', 'omit?', 'omit-reason', 'new?',
 	tagline = "Lily was here 2.19.52 -- automatically converted from ABC"
 	title = "Banish Misfortune"
 }
-'book' is also used in at least one file
 '''
 
-csv_keys_the_session = ['id', 'tune-id', 'setting-id', 'parse-order',
-    'omit?', 'omit-reason', 'new?', 'error-status?', 'flagged?',
-    'cn-code', 'ly-version', 'path', 'filename', 'mtime',
-    # these are the actual header fields in the session ly files
-    'title', 'subtitle', 'crossRefNumber', 'tagline', 'footnotes', 'book',
-    # maybe some files use these, so add them for good measure?
-    'composer']
+def get_csv_keys(mode):
+    lookup = {
+        'mutopia': ['mutopia-id', 'parse-order', 'omit?', 'omit-reason', 'new?', 'error-status?', 'flagged?',
+                    'cn-code', 'ly-version', 'mutopiacomposer', 'cn-title', 'cn-opus', 'path', 'filename',
+                    'cn-style', 'cn-instrument',
+                    'cn-poet', 'license-type', 'license-vsn', 'cn-license', 'mtime',
+                    'arranger', 'date', 'source', 'copyright', 'tagline',
+                    'footer', 'composer', 'mutopiatitle', 'title', 'mutopiaopus', 'opus', 'mutopiastyle', 'style',
+                    'mutopiainstrument', 'instrument', 'mutopiapoet', 'poet', 'mutopialicense', 'license'],
 
+        'thesession': ['id', 'tune-id', 'setting-id', 'parse-order',
+            'omit?', 'omit-reason', 'new?', 'error-status?', 'flagged?',
+            'cn-code', 'ly-version', 'path', 'filename', 'mtime',
+            # these are the actual header fields in the session ly files,
+            # book and footnotes are rarely used
+            'title', 'subtitle', 'crossRefNumber', 'tagline', 'footnotes', 'book',
+            # maybe some files use these, so include them for good measure
+            'composer']
+    }
+    return lookup[mode]
 
 def add_cn_fields(row):
-    # use separate 'cn-' fields so we can see what's going on in the .ly files
+    # use separate 'cn-' fields so we can easily see what's going on in the .ly files
     row['cn-title'] = row['mutopiatitle'] or row['title']
     row['cn-opus'] = row['mutopiaopus'] or row['opus']
     row['cn-style'] = row['mutopiastyle'] or row['style']
@@ -66,54 +69,41 @@ def add_license_data(row):
     row['license-vsn'] = license_lookup.get(row['cn-license'], ['', 0])[1]
     return row
 
-def process_ly_names(lyfilenames, dirpath, rootdir, mode, csv_keys):
+def omit_conflicts(row, conflicts):
+    relevant_conflicts = conflicts.copy()
+    # some conflicts have been omitted earlier
+    irrelevant_conflicts = ['composer', 'title', 'instrument', 'style', 'license', 'opus', 'poet']
+    for irr in irrelevant_conflicts:
+        # don't bother with a conflict if there's a mutopia___ version of it
+        if irr in relevant_conflicts and row['mutopia' + irr] != '':
+            relevant_conflicts.discard(irr)
+    return relevant_conflicts
 
-    # print('\n\n', version, dirpath[len(rootdir):], '\n', lyfilenames)
-
-    row, conflicting_data = get_header_data(lyfilenames, dirpath, csv_keys)
+def make_row(lyfilenames, dirpath, rootdir, mode, csv_keys):
+    # print('\n\n', dirpath[len(rootdir):], '\n', lyfilenames)
+    row, conflicts = get_header_data(lyfilenames, dirpath, csv_keys)
 
     # the files that are not included are top level files (usually just one file)
     included_files = get_included_files(lyfilenames, dirpath)
     not_included_files = list(set(lyfilenames).difference(included_files))
 
-    # print('included: ', included_files)
-    # print('not-included: ', not_included_files)
-
     # check for clairnote-code.ly in top-level files
-    clntSet = check_for_clairnote_code(not_included_files, dirpath)
+    clnt_set = check_for_clairnote_code(not_included_files, dirpath)
 
-    if len(clntSet) > 1:
-        conflicting_data.add('cn-code')
+    if len(clnt_set) > 1:
+        conflicts.add('cn-code')
         # TODO: confirm this output is what we want in the CSV file
-        row['cn-code'] = list(clntSet)
+        row['cn-code'] = list(clnt_set)
     else:
-        row['cn-code'] = list(clntSet)[0]
+        row['cn-code'] = list(clnt_set)[0]
 
     # Handle conflicting data in different files
-    # first prune irrelevant conflicts
-    # some are handled earlier, not even added to conflicting_data
-    if conflicting_data:
-        irrelevant_conflicts = ['composer', 'title', 'instrument', 'style', 'license', 'opus', 'poet']
-        for k in irrelevant_conflicts:
-            if k in conflicting_data and row['mutopia' + k] != '':
-                conflicting_data.discard(k)
-
-    # second deal with remaining relevant conflicts
-    if conflicting_data:
-        print('omit! conflicting data:', dirpath[len(rootdir):], list(conflicting_data), '\n')
+    # omit irrelevant conflicts, then deal with remaining relevant conflicts
+    relevant_conflicts = omit_conflicts(row, conflicts) if conflicts else []
+    if relevant_conflicts:
+        print('omit! conflicting data:', dirpath[len(rootdir):], list(relevant_conflicts), '\n')
         row['omit?'] = 'T'
-        row['omit-reason'] = 'conflicting header data: ' + repr(conflicting_data)
-
-    if mode == 'mutopia':
-        # extract mutopia-id from footer field
-        row['mutopia-id'] = regex_search(regexes['muto_id'], row['footer']) or ''
-        row = add_cn_fields(row)
-        row = add_license_data(row)
-    elif mode == 'thesession':
-        m = regexes['the_session_id'].search(row['subtitle'])
-        row['tune-id'] = m.group(1)
-        row['setting-id'] = m.group(2)
-        row['id'] = m.group(1) + '-' + m.group(2)
+        row['omit-reason'] = 'conflicting header data: ' + repr(relevant_conflicts)
 
     row['mtime'] = get_most_recent_mtime(lyfilenames, dirpath)
     row['filename'] = ',,, '.join(list(not_included_files))
@@ -121,108 +111,112 @@ def process_ly_names(lyfilenames, dirpath, rootdir, mode, csv_keys):
     row['path'] = dirpath[len(rootdir):].lstrip(os.path.sep)
 
     if mode == 'mutopia':
+        # extract mutopia-id from footer field
+        row['mutopia-id'] = regex_search(regexes['muto_id'], row['footer']) or ''
+        row = add_cn_fields(row)
+        row = add_license_data(row)
         print(dirpath, "   ", rootdir, "   ", row['path'])
 
-    # not currently used
-    # ftr = row['footer']
-    # row['mutopia-id'] = regex_search(regexes['muto_id'], ftr)
-    # strip footer to just the date
-    # row['footer'] = regex_search(regexes['footer'], ftr)
-    # remove footer
-    # row.pop('footer', False)
+    elif mode == 'thesession':
+        match = regexes['the_session_id'].search(row['subtitle'])
+        row['tune-id'] = match.group(1)
+        row['setting-id'] = match.group(2)
+        row['id'] = match.group(1) + '-' + match.group(2)
 
-    return row, conflicting_data
+    return row, relevant_conflicts
 
-def walk(rootdir, earliest_ly_version, mode, csv_keys):
-    """ Walk through rootdir and sub-directories parsing data from ly files.
-
-        In 'mutopia' mode we descend until we find a directory with ly files.
-        If there are subdirectories, we bail out and skip that piece.  Otherwise
-        we work on the ly files in that directory as all part of a single piece.
-
-        In 'thesession' mode we work with individual ly files as single pieces,
-        at any level in the hierarchy (usually just a directory of ly files).
-
-        rootdir (string) is path to the root directory
-        earliest_ly_version (string) (e.g. "2.19.52") we omit any ly files earlier than this version
-        mode (string) what mode we're in (e.g. 'mutopia', 'thesession')
-        csv_keys (list) the keys to use in the csv file we will create, varies with mode """
-    csv_data = []
-    parse_order = 0
-    conflicting_data_count = 0
-    subdir_skips = 'not applicable' if mode == 'thesession' else 0
-
+def get_ly_file_paths(rootdir):
+    result = []
     for dirpath, dirnames, filenames in os.walk(rootdir):
         if filenames != []:
+            # get list of all ly filnames in directory
+            lyfilenames = get_ly_filenames(filenames)
+            if lyfilenames != []:
 
-            if mode == 'mutopia':
-                # get list of all ly, ily, lyi filnames in directory
-                lyfilenames = get_all_lilypond_filenames(filenames)
-                if lyfilenames != []:
-                    if dirnames == []:
-                        version = get_version(lyfilenames, dirpath)
-                        if version != None and vsn_greater_than_or_equals(earliest_ly_version, version):
+                for name in lyfilenames:
+                    result.append({
+                        'path': dirpath,
+                        'lyfilenames': [name],
+                        'version': get_version([name], dirpath)
+                    })
+    return result, None
 
-                            row, conflicting_data = process_ly_names(lyfilenames, dirpath, rootdir, mode, csv_keys)
-                            row['ly-version'] = version
-                            parse_order += 1
-                            row['parse-order'] = parse_order
-                            csv_data.append(row)
+def get_mutopia_ly_file_paths(rootdir):
+    result = []
+    subdir_skips = 0
+    for dirpath, dirnames, filenames in os.walk(rootdir):
+        if filenames != []:
+            # get list of all ly, ily, lyi filenames in directory
+            lyfilenames = get_all_lilypond_filenames(filenames)
+            if lyfilenames != []:
 
-                            if (len(conflicting_data) > 0):
-                                conflicting_data_count += 1
+                if dirnames == []:
+                    result.append({
+                        'path': dirpath,
+                        'lyfilenames': lyfilenames,
+                        'version': get_version(lyfilenames, dirpath)
+                    })
+                else:
+                    # there are subdirectories and we can't handle that, so skip this piece.
+                    subdir_skips += 1
+                    # print('skip! subdirectories:', dirpath[len(rootdir):], dirnames, '\n')
+                    # clear dirnames to prevent os.walk from going deeper
+                    # this is how it's done, delete in place:
+                    dirnames.clear()
 
-                    else:
-                        # there are subdirectories and we can't handle them, so skip it.
-                        subdir_skips += 1
-                        # print('skip! subdirectories:', dirpath[len(rootdir):], dirnames, '\n')
-                        # clear dirnames to prevent os.walk from going deeper
-                        # we have to do it like this, delete in place:
-                        dirnames.clear()
+    return result, subdir_skips
 
-            elif mode == 'thesession':
-                # get list of all ly filnames in directory
-                lyfilenames = get_ly_filenames(filenames)
-                if lyfilenames != []:
-                    for name in lyfilenames:
-                        version = get_version([name], dirpath)
-                        if version != None and vsn_greater_than_or_equals(earliest_ly_version, version):
+def version_check(earliest_ly_version):
+    def vcheck(piece):
+        vsn = piece['version']
+        return vsn != None and vsn_greater_than_or_equals(earliest_ly_version, vsn)
+    return vcheck
 
-                            row, conflicting_data = process_ly_names([name], dirpath, rootdir, mode, csv_keys)
-                            row['ly-version'] = version
-                            parse_order += 1
-                            row['parse-order'] = parse_order
-                            csv_data.append(row)
+def handle_pieces(pieces, rootdir, mode, csv_keys):
+    csv_data = []
+    parse_order = 0
+    conflicts_count = 0
+    for p in pieces:
+        row, conflicts = make_row(p['lyfilenames'], p['path'], rootdir, mode, csv_keys)
+        row['ly-version'] = p['version']
+        parse_order += 1
+        row['parse-order'] = parse_order
+        csv_data.append(row)
 
-                            if (len(conflicting_data) > 0):
-                                conflicting_data_count += 1
-
-    return csv_data, parse_order, conflicting_data_count, subdir_skips
+        if (len(conflicts) > 0):
+            conflicts_count += 1
+    return csv_data, parse_order, conflicts_count
 
 def main(args):
     try:
         if args.mode == 'mutopia':
-            csv_keys = csv_keys_mutopia
+            file_paths_func = get_mutopia_ly_file_paths
         elif args.mode == 'thesession':
-            csv_keys = csv_keys_the_session
+            file_paths_func = get_ly_file_paths
         else:
-            raise ValueError("Need a valid mode argument, either 'mutopia' or 'thesession'.")
+            raise ValueError("Oops! We need a valid mode argument, either 'mutopia' or 'thesession'.")
 
-        csv_data, parse_order, conflicts, skips = walk(args.rootdir, args.earliest_ly_version, args.mode, csv_keys)
+        csv_keys = get_csv_keys(args.mode)
+
+        pieces, subdir_skips = file_paths_func(args.rootdir)
+        pieces_version_checked = list(filter(version_check(args.earliest_ly_version), pieces))
+
+        csv_data, parse_order, conflicts_count = handle_pieces(pieces_version_checked, args.rootdir, args.mode, csv_keys)
 
         print('\nLilyPond files parsed, data gathered.',
-            '\n  Total works:', parse_order,
-            '\n  conflicting_data_count:', conflicts,
-            '\n  subdir_skips:', skips)
+              '\nTotal works:', parse_order,
+              '\nconflicting data count:', conflicts_count,
+              '\nsubdirectory skips:', subdir_skips or 'not applicable', '(skipped pieces that have hierarchical directories of ly files)')
 
         # MERGE IN PREVIOUS CSV META DATA AND MARK NEW ITEMS
+        # TODO: use 'id' instead of 'mutopia-id' ? requires data migration...
         id_field = 'mutopia-id' if args.mode == 'mutopia' else 'id'
 
         final_csv_data = merge_csv_data(args.csv_previous, csv_data, id_field) if args.csv_previous else csv_data
 
         # GENERATE CSV FILE
-        with open(args.csv_output, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_keys)
+        with open(args.csv_output or 'csv-output.csv', 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames = csv_keys)
             writer.writeheader()
             for line in final_csv_data:
                 writer.writerow(line)
