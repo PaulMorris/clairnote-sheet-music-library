6dcc829e421c432ba7a0e201d9c6a63a0c3898c2
Paul Morris <paul@paulwmorris.com>
2017-04-11 00:15:47 -0400

    Add batch_abc2ly.py file

---
+++ batch_abc2ly.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import subprocess, os, argparse
+from console_utils import run_command, log_lines, print_lines
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--filecount", help="Only convert this many files")
+parser.add_argument("logfile", help = "Log console output to this file")
+parser.add_argument("errorfile", help="Log errors to this file")
+parser.add_argument("indir", help="Read files from this directory")
+parser.add_argument("outdir", help = "Write the converted files to this directory")
+
+def main(args):
+    error_summary = ['', 'ERROR SUMMARY', '']
+    for dirpath, dirnames, filenames in os.walk(args.indir):
+        if filenames:
+            # get list of all .abc files in directory
+            abcs = filter(lambda name: name[-4:] == '.abc', filenames)
+            abc_filenames = list(abcs)[:int(args.filecount)] if args.filecount else abcs
+
+            for name in abc_filenames:
+                readpath = os.path.join(dirpath, name)
+                writepath = os.path.join(args.outdir, name[0:-4] + '.ly')
+
+                returncode, console_out = run_command([
+                    'abc2ly',
+                    '--output=' + writepath,
+                    '--strict',
+                    readpath
+                ])
+
+                console_out_returncode = console_out[:-1] + [str(returncode)]
+                log_lines(console_out_returncode, args.logfile)
+                print_lines(console_out_returncode)
+
+                if returncode == 1:
+                    error_summary.append(readpath)
+                    log_lines(console_out, args.errorfile)
+
+    log_lines(error_summary, args.errorfile)
+
+if __name__ == "__main__":
+    main(parser.parse_args())
